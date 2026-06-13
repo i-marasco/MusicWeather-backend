00e89d47193ba0803ebaa3c2422148494b6864ee
Imarasco <ivan.marasco@icloud.com>
2026-06-13 11:27:54 +0200

    refactor(secret): Remove secret

--- app/database/conn.py
+++ app/database/conn.py
@@ -1,4 +1,10 @@
+import os
 import psycopg2
+
+from dotenv import load_dotenv
+
+# Load variables from .env into environment
+load_dotenv()
 
 def get_connection():
     """
@@ -6,9 +12,9 @@ def get_connection():
             Connection to PostgreSQL database.
     """
     return psycopg2.connect(
-        dbname="music",
-        user="postgres",
-        password="admin",
-        host="localhost",
-        port="5432"
+        dbname=os.getenv("DB_NAME"),
+        user=os.getenv("DB_USER"),
+        password=os.getenv("DB_PASSWORD"),
+        host=os.getenv("DB_HOST"),
+        port=os.getenv("DB_PORT")
     )
