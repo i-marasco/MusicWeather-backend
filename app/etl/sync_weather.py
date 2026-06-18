7dafd4a58873d73278511fb97ba6f30e11a44152
Imarasco <ivan.marasco@icloud.com>
2026-06-18 22:37:37 +0200

    fix(api): Change Open Weather api with Open Meteo

--- app/etl/sync_weather.py
+++ app/etl/sync_weather.py
@@ -1,18 +1,3 @@
-"""
--------------------------------------------------------------------------------------------------
-sync_weather.py
--------------------------------------------------------------------------------------------------
-Purpose:
-
-Source:
-
-Input:
-
-Output:
-
-Note:
-
-"""
 from datetime import datetime
 import requests
 import os
@@ -21,71 +6,69 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-OPENWEATHER_API_KEY = os.getenv("OPENWEATHER_API_KEY")
-OPENWEATHER_LAT = os.getenv("OPENWEATHER_LAT")
-OPENWEATHER_LON = os.getenv("OPENWEATHER_LON")
+OPENMETEO_LAT = os.getenv("OPENMETEO_LAT")
+OPENMETEO_LON = os.getenv("OPENMETEO_LON")
+OPENMETEO_URL = os.getenv("OPENMETEO_URL")
 
-OPENWEATHER_URL = "https://api.openweathermap.org/data/2.5/weather"
+def get_weather_history():
+    params = {
+        "latitude": OPENMETEO_LAT,
+        "longitude": OPENMETEO_LON,
+        "start_date": "2025-01-01",
+        "end_date": "2026-01-01",
+        "hourly": [
+            "temperature_2m",
+            "relative_humidity_2m",
+            "pressure_msl",
+            "wind_speed_10m",
+            "weather_code"
+        ],
+        "timezone": "auto"
+    }
+
+    r = requests.get(OPENMETEO_URL, params=params)
+    r.raise_for_status()
+    return r.json()
 
 
 def sync_weather():
+    data = get_weather_history()
+    hourly = data["hourly"]
 
-    params = {
-        "lat": OPENWEATHER_LAT,
-        "lon": OPENWEATHER_LON,
-        "appid": OPENWEATHER_API_KEY,
-        "units": "metric"
-    }
-
-    r = requests.get(OPENWEATHER_URL, params=params)
-    data = r.json()
-    print(data)
+    times = hourly["time"]
 
     conn = get_connection()
 
     try:
         with conn.cursor() as cur:
 
-            cur.execute("""
-                INSERT INTO "WEATHER"."WEATHER_HISTORY" (
-                    "OBSERVED_AT",
-                    "CITY",
-                    "LAT",
-                    "LON",
-                    "TEMPERATURE",
-                    "FEELS_LIKE",
-                    "HUMIDITY",
-                    "PRESSURE",
-                    "WEATHER_MAIN",
-                    "WEATHER_DESCRIPTION",
-                    "WIND_SPEED",
-                    "CLOUDINESS",
-                    "SUNRISE",
-                    "SUNSET"
-                )
-                VALUES (
-                    %s, %s, %s, %s,
-                    %s, %s, %s, %s,
-                    %s, %s,
-                    %s, %s,
-                    %s, %s
-                );
-            """, (
-                datetime.now(),
-                data.get("name"),
-                data["coord"]["lat"],
-                data["coord"]["lon"],
-                data["main"]["temp"],
-                data["main"]["feels_like"],
-                data["main"]["humidity"],
-                data["main"]["pressure"],
-                data["weather"][0]["main"],
-                data["weather"][0]["description"],
-                data["wind"]["speed"],
-                data["clouds"]["all"],
-                datetime.fromtimestamp(data["sys"]["sunrise"]),
-                datetime.fromtimestamp(data["sys"]["sunset"])
-            ))
+            for i in range(len(times)):
+
+                cur.execute("""
+                    INSERT INTO "WEATHER"."WEATHER_HISTORY" (
+                        "OBSERVED_AT",
+                        "CITY",
+                        "LAT",
+                        "LON",
+                        "TEMPERATURE",
+                        "HUMIDITY",
+                        "PRESSURE",
+                        "WIND_SPEED",
+                        "WEATHER_CODE"
+                    )
+                    VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s);
+                """, (
+                    datetime.fromisoformat(times[i]),
+                    "Milan",
+                    float(OPENMETEO_LAT),
+                    float(OPENMETEO_LON),
+
+                    hourly["temperature_2m"][i],
+                    hourly["relative_humidity_2m"][i],
+                    hourly["pressure_msl"][i],
+                    hourly["wind_speed_10m"][i],
+                    hourly["weather_code"][i]
+                ))
 
             conn.commit()
 
