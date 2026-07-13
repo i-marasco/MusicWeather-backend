7671879905e7eaf6b4f2eca897235bf33354977b
Imarasco <ivan.marasco@icloud.com>
2026-07-13 21:34:29 +0200

    feat(api): Add Vue.js connector

--- main.py
+++ main.py
@@ -1,11 +1,25 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
 from app.api.listening import router as listening_router
 from app.api.artist import router as artist_router
 from app.api.artist_ranking import router as artist_ranking_router
 from app.api.genres import router as genres_router
 from app.api.genre_ranking import router as genre_ranking_router
 from app.api.weather import router as weather_router
+
 app = FastAPI(title="Spotify Weather API")
+
+# Allow Vue frontend to communicate with FastAPI
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "http://localhost:5173",
+    ],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(listening_router, prefix="/listening", tags=["Listening"])
 app.include_router(artist_router, tags=["Artist"])
